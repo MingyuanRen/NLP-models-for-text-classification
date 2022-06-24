392d44964b43708ab2a4a5414a8a55bdd9da4e57
Mingyuan Ren <m28ren@uwaterloo.ca>
2022-06-24 11:26:35 -0400

    textrnn

---
+++ TextRNN.py
@@ -0,0 +1,23 @@
+from tensorflow.keras import Input, Model
+from tensorflow.keras.layers import Embedding, Dense, Dropout, LSTM
+
+
+class TextRNN(object):
+    def __init__(self, maxlen, max_features, embedding_dims,
+                 class_num=5,
+                 last_activation='softmax'):
+        self.maxlen = maxlen
+        self.max_features = max_features
+        self.embedding_dims = embedding_dims
+        self.class_num = class_num
+        self.last_activation = last_activation
+
+    def get_model(self):
+        input = Input((self.maxlen,))
+
+        embedding = Embedding(self.max_features, self.embedding_dims, input_length=self.maxlen)(input)
+        x = LSTM(128)(embedding)
+
+        output = Dense(self.class_num, activation=self.last_activation)(x)
+        model = Model(inputs=input, outputs=output)
+        return model
